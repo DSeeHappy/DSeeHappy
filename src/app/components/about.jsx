cbd3db297e208939b0ebbe1f8c985a423c8c0245
Daniel Sanchez <ds@dseehappy.com>
2023-08-14 14:28:46 -0600

    about me segment

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -1,71 +1,199 @@
 'use client'
 
-
 import Image from "next/image";
+import {
+    ArrowPathIcon, CameraIcon,
+    ChatBubbleLeftIcon, CodeBracketSquareIcon,
+    CogIcon, CubeTransparentIcon,
+    DevicePhoneMobileIcon,
+    DeviceTabletIcon, DocumentTextIcon, LightBulbIcon, PaintBrushIcon,
+    SwatchIcon, UserCircleIcon
+} from "@heroicons/react/24/solid";
 
 export default function About(){
     return (
         <>
             <section
+                className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0 -z-20"
                 id="about">
-
-                <div className="relative isolate overflow-hidden pt-14  ">
-                    <img
-                        src="/scenery/breckmountains.png"
-                        alt="Mountain in Breckenridge photo by DSeeHappy"
-                        className="absolute inset-0 -z-10 h-full w-full object-cover"
-                    />
-                    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                            <div
-                                className="relative rounded-full px-3 py-1 text-sm leading-6 text-white bg-gradient-to-r from-cyan-500 to-blue-500">
-                                Software Developer experienced in Flutter | Android | Web
-                            </div>
-                        </div>
-                        <div className="text-center">
-                            <div
-                                className="flex items-center rounded-tr-3xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
-                                <Image
-                                    className="mx-auto h-48 w-48 rounded-ee-full md:h-56 md:w-56 object-cover scale-110 shadow-2xl "
-                                    src='/me/Daniel-Sanchez.webp' height={500} width={500}
-                                    alt=""/>
-                                <div className="align-middle">
-                                    <h1 className="text-3xl font-bold tracking-tight text-white sm:text-5xl ">
-                                        Hi I am Juan Daniel Sanchez Chavez
-                                    </h1>
-                                    <br/>
-                                    <h2 className="text-3xl font-bold tracking-tight text-white sm:text-5xl ">
-                                        Call me Daniel
-                                    </h2>
-                                </div>
-                            </div>
-                            <div className=" grid grid-cols-1 divide-y rounded-bl-3xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
-                                <p className="p-5 text-3xl leading-8 text-white ">
-                                    I&apos;m a software developer with proficiency in Android, Flutter, and web
-                                    development.
-                                </p>
-                                <p className="text-center pt-10 m-6 text-2xl leading-8 text-white ">
-                                    Passionate about design, testing, and ensuring decisive communication between team members
-                                    and
-                                    stakeholders.
-                                </p>
-                                <p className="p-4 text-3xl leading-8 text-white ">
-                                    Committed to crafting customer focused solutions
+                <div className="absolute inset-0 -z-10 overflow-hidden">
+                    <svg
+                        className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+                        aria-hidden="true"
+                    >
+                        <defs>
+                            <pattern
+                                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+                                width={200}
+                                height={200}
+                                x="50%"
+                                y={-1}
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <path d="M100 200V.5M.5 .5H200" fill="none"/>
+                            </pattern>
+                        </defs>
+                        <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+                            <path
+                                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+                                strokeWidth={0}
+                            />
+                        </svg>
+                        <rect width="100%" height="100%" strokeWidth={0}
+                              fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"/>
+                    </svg>
+                </div>
+                <div
+                    className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+                    <div
+                        className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                        <div className="lg:pr-4">
+                            <div className="lg:max-w-lg">
+                                <p className="text-base font-semibold leading-7 text-indigo-600">About Me</p>
+                                <h3 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Software
+                                    & Photography</h3>
+                                <p className="mt-6 text-xl leading-8 text-gray-700">
+                                    My enjoyment of software is only rivaled by snowboarding and photography
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div
-                        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu bg-gray-200/5 overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                        aria-hidden="true"
-                    >
-                        <div
-                            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] bg-blue-900"
-                            style={{
-                                clipPath:
-                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                            }}
+                        className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                        <Image
+                            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+                            src="/scenery/wptree.png"
+                            height={500}
+                            width={500}
+                            alt="Tree in Winter Park photo by DSeeHappy"
                         />
+                    </div>
+                    <div
+                        className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                        <div className="lg:pr-4">
+                            <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+
+                                <p className=" text-3xl text-black leading-5 font-semibold tracking-tight">Skills</p>
+                                <ul role="list" className="mt-8 space-y-4 text-gray-600 gri ">
+                                    <li className="flex gap-x-3">
+                                        <SwatchIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                    aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Photoshop</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <ChatBubbleLeftIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                            aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Spanish</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <DeviceTabletIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                          aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Dart | Flutter Mobile Development</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <DevicePhoneMobileIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                               aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Kotlin | Android Development</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CodeBracketSquareIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                               aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Python</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <PaintBrushIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                        aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">HTML | CSS</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <DocumentTextIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                          aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">JavaScript | TypeScript</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <LightBulbIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                       aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">React</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <ArrowPathIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                       aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">NextJs</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CubeTransparentIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                             aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Gatsby</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <UserCircleIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                        aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">UI | UX</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CogIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                 aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Go</strong>
+                  </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CameraIcon className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                                    aria-hidden="true"/>
+                                        <span>
+                    <strong className="font-semibold text-gray-900">Photography</strong>
+                  </span>
+                                    </li>
+                                </ul>
+                                <h4 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Continuous
+                                    Learning</h4>
+                                <p className="mt-8">
+                                    Technology progresses at a pace that&apos;s often breathtaking, I enjoy and seek
+                                    this challenge. Every time I explore my current limits, I discover more to learn. As
+                                    long as I continue to learn and study, I will reach a level I never imagined
+                                    possible.
+                                </p>
+                                <h4 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Collaborative
+                                    Mindset
+                                </h4>
+                                <p className="mt-6">
+                                    Transitioning from a background in customer service to software development has been
+                                    an unexpected asset. This foundation gives me a unique perspective on user needs,
+                                    management concerns, and tight deadlines. It equips me to collaborate effectively
+                                    with stakeholders and team members, ensuring we prioritize the most impactful
+                                    solutions and avoid costly missteps.
+                                </p>
+                                <h4 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Creativity Mindset
+                                </h4>
+                                <p className="mt-6">
+                                    Some see software development as just analytical, but I find creativity essential.
+                                    It&apos;s key in designing solutions, understanding user needs, and ensuring our products
+                                    not only work well but also appeal to users.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image'
 import About from "@/app/components/about";
 import Projects from "@/app/components/projects";
 import Contact from "@/app/components/contact";
+import Introduction from "@/app/components/introduction";
 
 export default function Home() {
     return (
         <main>
+            <Introduction/>
             <About/>
             <Projects/>
             <Contact/>
